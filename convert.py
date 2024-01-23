0b1f81595624ee0dfcc0be2a99d9ba5b550682ac
Lecoustre Malcom <malcom.lecoustre@gmail.com>
2024-01-23 15:31:08 +0100

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+
+def compress_to_love(directory, output_filename):
+    # Assurez-vous que le nom de fichier de sortie se termine par .love
+    if not output_filename.endswith('.love'):
+        output_filename += '.love'
+
+    # Utilisez shutil pour faire une archive zip
+    shutil.make_archive(output_filename, 'zip', directory)
+
+    # Renommez l'archive zip en .love
+    os.rename(output_filename + '.zip', output_filename)
+
+def select_directory():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    directory = filedialog.askdirectory()  # Show the dialog to choose directory
+    compress_to_love(directory, 'jeu')  # Compress the chosen directory
+
+select_directory()
